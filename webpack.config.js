19dd6c59ac81b5017c21edb9a5bd603af72a07f8
mino <yu.wang@oriente.com>
2018-06-28 11:11:08 +0800

    初始化cordova

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+    entry: "./index.js",
+    output:{
+        path: path.resolve(__dirname, "dist"), 
+         filename: "cordova.js",
+    },
+    module:{
+      rules:[
+      {
+        test: /\.jsx?$/,
+        include: [
+          path.resolve(__dirname, "lib"),
+          path.resolve(__dirname, "index.js")
+        ],
+        exclude:/node_modules/,
+        loader: "babel-loader",
+        // the loader which should be applied, it'll be resolved relative to the context
+        // -loader suffix is no longer optional in webpack2 for clarity reasons
+        // see webpack 1 upgrade guide
+
+      },
+      ]  
+    }
+}
