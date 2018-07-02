6a2332bb70059f8f17f26eaf7442d0e0909f996c
TaoYu <121595113@qq.com>
2018-07-02 14:38:06 +0800

    拆分原有模块

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-import channel from './lib/channel'
-import Cordova from './lib/cordova'
+// import channel from './lib/channel'
+// import Cordova from './lib/cordova'
 
-window.cordova = new Cordova();
+// window.cordova = new Cordova();
+
+// import {require, define} from './source/cordova-ios.js';
+
+// import  './plugins/track.js';
+// import aa from './plugins/fetchdata.js';
+
+// import pluginA from "pluginA";
+
+// cordova.注册('pluginA', pluginA);
+// console.log(111, aa)
+
+// console.log([require(aa)])
+
+// // console.log(require('cordova/channel'))
+
+// require('cordova/init');
+
+import cordova from './lib/cordova2';
+
+window.cordova = cordova;
+import './lib/init';
---
+++ pluginA.js
@@ -0,0 +1,20 @@
+class PluginA {
+  constructor(option) {
+    // code
+  }
+
+  created (cordova) {
+  	console.log(11111)
+  }
+
+  cordovaReady(){
+
+  }
+  fetchDataFromNative (successFn, errorFn) {
+
+  }
+
+  // methods
+}
+
+ export default PluginA;
