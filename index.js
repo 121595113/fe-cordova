218b3f12dd40429d9ffc3595a4d829301ebf3e3b
TaoYu <121595113@qq.com>
2018-07-12 16:43:16 +0800

    删除注释代码

--- index.js
+++ index.js
@@ -1,11 +1,2 @@
-// import Cordova from './lib/cordova'
-
-// import pluginA from "pluginA";
-
-// Cordova.registerPlugin('pluginA', pluginA);
-
-// window.cordova = new Cordova();
-
 export { default as Cordova } from './lib/cordova';
-
 export { default as Plugin } from './lib/plugin'
